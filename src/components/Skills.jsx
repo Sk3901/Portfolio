912c3559fd88cb46f8639dba3845ef6ab6455934
Samu <Samu.kirjonen@gmail.com>
2024-11-13 16:31:09 +0200

    modify skills section

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -11,6 +11,11 @@ export default class infiniteSlider{
         this.outerContainer.sideGradientRGB = config.outerContainer.sideGradientRGB || '';
         this.outerContainer.element = null;
 
+        this.setInnerContainer = {};
+        this.setInnerContainer.selector = 'inf_slider_inner_container';
+        this.setInnerContainer.array = [];
+        this.setInnerContainer.width = null
+
         this.title= config.title || {};
         this.title.text = config.title.text || null;
         this.title.fontSize = config.title.fontSize || 74;
@@ -39,20 +44,145 @@ export default class infiniteSlider{
     }
 
     setOuterContainer() {
+        this.outerContainer.element = document.querySelector(this.outerContainer.selector);
 
 
     }
     setInnerContainerWidth() {
+        this.setInnerContainer.width = this.elements.items.length * (this.elements.width + (this.elements.margin * 2));
+
 
     }
     setInnerContainer(){
+        var containerAmount = Math.ceil(this.outerContainer.maxWidth / this.innerContainer.width) + 1;
+        for (var i=0; i < containerAmount; i++) {
+            let containerHTML = '';
+            let elemntsHTML = '';
+            let containerSelector = `${this.innerContainer.selector.replaceAll('.','')}--${i}`;
+
+
+            for (var item of thisq.elements.items) {
+                elementsHTML += `
+                ${!item.src
+                ? 'div class="inf-slider__slide">' + item.content + '</div>'
+                : '<div class="inf-slider__slide"><img src ="' + item.src + '></div>'
+            }`
+            }
+            containerHTML = `
+            <div class="${this.innerContainer.selector.replaceAll('.', '')} ${containerSelector}>
+                ${elemntsHTML}
+
+            </div>
+            `
+
+            this.innerContainer.array.push({
+                containerHTML: containerHTML,
+                contaierSelector: containerSelector
+            })
+        }
 
     }
     insertCSS(){
+        var styletag = document.createElement('style');
+        styletag.id = 'inf-slider-css';
+        styletag.innerHTML = `
+        @import url('https://fonts.googleapis.com/css2?family=Open+Sans&display=swap');
 
+        ${this.outerContainer.selector}{
+            border: ${this.sliderControls.developmentMode ? '1px solid limegreen' : '' };
+            background: ${this.outerContainer.background};
+            position: relative;
+            width: 90%;
+            height: ${this.outerContainer.height}px;
+            max-width: ${this.outerContainer.maxWidth}px;
+            margin: 0 auto;
+            padding: ${this.outerContainer.padding}px;
+            overflow: hidden;
+            display: grid;
+        }
+
+        ${this.onnerContainer.selector}::before,
+        ${this.outerContainer.selector}::after{
+            content: '';
+            position: absolute;
+            height: 100%;
+            width: 100%;
+            top: 0;
+            left: 0;
+            background: ${this.outerContainer.sideGradientRGB.length > 0
+            ? 'linear-gradient(to right, rgba(' + this.outerContainer.sideGradientRGB + ', 1) 0%, rgba(' + this.outerContainer.sideGradientRGB + ', 0) 10%)'
+            : '' }
+        z-index: 5;
     }
-    insertHTML(){
+    ${this.outerContainer.selector}::after {
+        right: 0px;
+        background: ${this.outerContainer.sideGradientRGB.length > 0 
+            ? 'linear-gradient(to left, rgba(' + this.outerContainer.sideGradientRGB + ',1) 0%, rgba(' + this.outerContainer.sideGradientRGB + ',0) 10%)'  
+            : ''};
+    }
+    .inf-slider__show-title-wrapper {
+        display: block;
+        text-align: ${this.title.textAlign};
+    }
+    .inf-slider__hide-title-wrapper {
+        display: none;
+        overflow: hidden;
+    }
 
+    .inf-slider__title{
+        color: ${this.title.colorCode};
+        font-size: ${this.title.fontSize};
+        font-family: ${this.title.fontFamily || "'Open Sans', sans-serif"};
+        padding: ${this.title.padding}px;
+    }
+
+    ${this.innerContainer.selector}-wrapper {
+        position:relative;
+        height: 100%;
+        
+    }
+
+    ${this.innerContainer.selector} {
+        position: absolute;
+        top: 0;
+        display; grid;
+        grid-template-columns: repeat(${this.elements.items.length}, 1fr);
+        width: ${this.sliderControls.developmentMode ? 'red 1px solid' : ''};
+    }
+
+    .inf-slider__slide {
+        display: inline-block;
+        background: ${this.elements.background};
+        margin: ${this.elements.margin}px;
+        padding: ${this.elements.padding}px;
+        width: ${this.elements.width}px;
+        height: ${this.elements.width * 2};
+        font-family: ${this.title.fontFamily || "'Open Sans', sans-serif"};
+        position: relative;
+    }
+    .inf-slider__slide img {
+        width: 100%;
+        
+    }
+    `
+    document.head.appendChild(styletag);
+    };
+    
+
+    insertHTML(){
+        var innerSliderHTML = '';
+
+        this.outerContainer.element.innerHTML += `
+        <div class="${this.title.text ? 'inf-slider__show-title-wrapper' : 'inf-slider__hide-title-wrapper'}">
+        <h1 class="inf-slider__title">${this.title.text}</h1>
+
+        </div>
+
+        this.outerContainer.element.innerHTML += `
+        <div class="${this.innercontainer.selector.replaceAll('.','')}-wrapper">
+            ${innerSliderHTML}
+        </div>
+        `;
     }
     startAnimation(){
 
@@ -127,3 +257,4 @@ var sliderConfig = {
 
     }
 }
+
